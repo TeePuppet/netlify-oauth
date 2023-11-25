3cbf5b189a75d952e3680ec81ef04724ae5ce4ce
TeePuppet <silviu@silviu.co.uk>
2023-11-25 22:46:12 +0200

    update callback

--- api/callback.js
+++ api/callback.js
@@ -1,48 +1,49 @@
-const tiny = require('tiny-json-http');
+// /api/callback.js
+const tiny = require("tiny-json-http");
 
 module.exports = async (req, res) => {
-	const client_id = process.env.OAUTH_CLIENT_ID;
-	const client_secret = process.env.OAUTH_CLIENT_SECRET;
-	const tokenUrl = 'https://github.com/login/oauth/access_token';
+  const client_id = process.env.OAUTH_CLIENT_ID;
+  const client_secret = process.env.OAUTH_CLIENT_SECRET;
+  const tokenUrl = "https://github.com/login/oauth/access_token";
 
-	const data = {
-		code: req.query.code,
-		client_id,
-		client_secret,
-	};
+  const data = {
+    code: req.query.code,
+    client_id,
+    client_secret
+  };
 
-	try {
-		const { body } = await tiny.post({
-			url: tokenUrl,
-			data,
-			headers: { Accept: 'application/json' },
-		});
+  try {
+    const { body } = await tiny.post({ 
+      url: tokenUrl, 
+      data, 
+      headers: { "Accept": "application/json" } 
+    });
 
-		const postMsgContent = {
-			token: body.access_token,
-			provider: 'github',
-		};
+    const postMsgContent = {
+      token: body.access_token,
+      provider: "github"
+    };
 
-		const script = `
-        <script>
-        (function() {
-          function recieveMessage(e) {
-            console.log("recieveMessage %o", e);
-    
-            window.opener.postMessage(
-              'authorization:github:success:${JSON.stringify(postMsgContent)}', 
-              e.origin
-            );
-          }
-    
-          window.addEventListener("message", recieveMessage, false);
-          window.opener.postMessage("authorizing:github", "*");
-        })()
-        </script>`;
+    const script = `
+    <script>
+    (function() {
+      function recieveMessage(e) {
+        console.log("recieveMessage %o", e);
 
-		return res.send(script);
-	} catch (err) {
-		console.log(err);
-		res.redirect('/?error=😡');
-	}
+        window.opener.postMessage(
+          'authorization:github:success:${JSON.stringify(postMsgContent)}', 
+          e.origin
+        );
+      }
+
+      window.addEventListener("message", recieveMessage, false);
+      window.opener.postMessage("authorizing:github", "*");
+    })()
+    </script>`;
+
+    return res.send(script);
+  } catch (err) {
+    console.log(err);
+    res.redirect("/?error=😡");
+  }
 };
